d2490c262fa0819643bb0ad676b30429752fe585
Chris Hanks <christopher.m.hanks@gmail.com>
2016-01-22 14:44:28 -0500

    Replace facet calculation with a more efficient GROUP BY GROUPING SETS implementation that only requires a single DB query.

--- lib/sequel/extensions/full_text_search.rb
+++ lib/sequel/extensions/full_text_search.rb
@@ -15,11 +15,43 @@ module Sequel
       end
 
       def facets(columns, filters: {})
-        result = {}
+        ds = naked.unordered.group_by(*columns).grouping_sets
+
+        selections = columns.map { |column|
+          aggregate = Sequel.function(:count, Sequel.lit('*'))
+
+          if (filter = filters.except(column)).any?
+            if filter.length == 1
+              aggregate = aggregate.filter(filter)
+            else
+              raise
+              # ?
+              # aggregate = aggregate.filter(filter.inject{|a,b| ::Sequel::SQL::BooleanExpression.new(:AND, a, b)})
+            end
+          end
+
+          [column, aggregate.as("#{column}_count".to_sym)]
+        }
+
+        ds = ds.select(*selections.flatten)
+
+        results = {}
+
         columns.each do |column|
-          result[column] = unordered.group_by(column).where(filters.except(column)).select_hash(column, Sequel.as(SQL::Function.new(:count, Sequel.lit('*')), :count))
+          results[column] = {}
         end
-        result
+
+        # TODO: What to do about actually nil values?
+        ds.each do |row|
+          columns.each do |column|
+            value = row.fetch(column)
+            unless value.nil?
+              results[column][value] = row.fetch("#{column}_count".to_sym)
+            end
+          end
+        end
+
+        results
       end
     end
   end
